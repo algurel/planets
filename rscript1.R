9c5f978cd5659bb072bb8c6e117cf25f5c7badbe
Aleka Gurel <aleka.gurel@gmail.com>
2018-03-11 16:25:47 -0700

    added missing data

---
+++ rscript1.R
@@ -0,0 +1,4 @@
+family <- data.frame(member = c("mom", "dad", "baby"),
+                     age =c(36, 34, .5),
+                     profession = c("tennis player", "internet entrepreneur", "infant"))
+
