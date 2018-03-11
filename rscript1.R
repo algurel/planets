851c0fc7743eac940e956a2077da1fff3473b999
Aleka Gurel <aleka.gurel@gmail.com>
2018-03-11 16:37:13 -0700

    added a new vector

--- rscript1.R
+++ rscript1.R
@@ -2,3 +2,4 @@ family <- data.frame(member = c("mom", "dad", "baby"),
                      age =c(36, 34, .5),
                      profession = c("tennis player", "internet entrepreneur", "infant"))
 
+newvec <- c(3, 2, 1)
